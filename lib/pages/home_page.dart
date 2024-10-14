55179cbb7345b0f478cdda7f07a0cd7bdfc5f300
Renan <renanmoreiradev@outlook.com>
2024-10-14 15:12:56 -0300

    feat: initial task list structure

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List toDoList = [
+    ['Passear com cachorro', false],
+    ['Limpar escrivania', false],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Lista de Tarefas'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (BuildContext context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: 0,
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(
+                  toDoList[index][0],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
